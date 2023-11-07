cbd0b85d4c57190cdb3a4d1c6b521aa0f0a4acde
SantiJaime <santij03@gmail.com>
2023-11-07 12:48:26 -0300

    elimino check de put de productos

--- routes/productos.js
+++ routes/productos.js
@@ -35,23 +35,13 @@ router.post(
 );
 router.put(
   "/:id",
-  [
-    check("id", "Formato ID incorrecto").isMongoId(),
-    check("nombre", "El campo nombre está vacío").notEmpty(),
-    check(
-      "nombre",
-      "Mínimo de 3 caracteres | Máximo de 50 caracteres"
-    ).isLength({
-      min: 3,
-      max: 50,
-    }),
-    check("descripcion", "El campo descripción está vacío").notEmpty(),
-    check("categoria", "El campo categoría está vacío").notEmpty(),
-    check("imagen", "El campo imágen está vacío").notEmpty(),
-    check("precio", " El campo precio está vacío").notEmpty(),
-  ],
+  [check("id", "Formato ID incorrecto").isMongoId()],
   updateProduct
 );
-router.delete("/:id", [check("id", "Formato ID inválido").isMongoId()], deleteProduct);
+router.delete(
+  "/:id",
+  [check("id", "Formato ID inválido").isMongoId()],
+  deleteProduct
+);
 
 module.exports = router;
